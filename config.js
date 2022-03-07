45e7adba9fc426ab8fb604c011cd8a08ea00d52c
xoandaigia <101124043+xoandaigia@users.noreply.github.com>
2022-03-07 23:03:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "chúc em ngày 8/3",
+    introTitle: 'Nguyễn Thị Chuyên',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
