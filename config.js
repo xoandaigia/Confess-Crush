b76300061538c274a6744f9476428a55b9ec527b
xoandaigia <101124043+xoandaigia@users.noreply.github.com>
2022-03-07 23:11:19 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    titleWeb: "chúc em ngày 8/3",
+    introTitle: 'Nguyễn Thị Chuyên',
+    introDesc: `vậy là đã hơn 4 năm yêu nhau
+    đã có những vui buồn
+    nhưng cuối cùng chúng ta vẫn bên nhau
+    chúng ta cùng cố gắng tiếp tục nhé`,
+    btnIntro: '^^HeHe^^',
+    title: 'không biết sẽ đi đến đâu 🥰',
+    desc: 'nhưng anh sẽ cố gắng. em cũng vậy né ',
+    btnYes: 'vag ạ <33',
+    btnNo: 'Không :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'vì anh đjep trai mà',
+    mess: 'anh biết mà 🥰. Yêu em 😘😘',
+    messDesc: 'chúc em ngày 8/3 vui vẻ. hanh phúc. thành công. xinh đẹp.',
+    btnAccept: 'cảm ơn anh <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
